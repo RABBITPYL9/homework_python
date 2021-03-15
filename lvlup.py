7e66623baa2e3ad9e36b25b03cff693eb1d18ee4
RABBITPYL9 <shubins317@gmail.com>
2021-03-15 09:09:56 +0300

    THIS IS SPARTA!

---
+++ lvlup.py
@@ -0,0 +1,11 @@
+student = {'first_name': 'Иван', 'last_name': 'Иванов', 'age': 34, 'cities': ['Лондон', 'Париж', 'Нью Йорк']}
+
+print(student['first_name'])                  # Иван
+print(f"фамилия: {student['last_name']}")     # фамилия: Иванов
+print(f"возраст: {student['age']}")           # возраст: 34
+print(f"посещал города: {student['cities']}") # посещал города: ['Лондон', 'Париж', 'Нью Йорк']
+
+student = {'first_name': 'Иван', 'last_name': 'Иванов', 'age': 34, 'cities': ['Лондон', 'Париж', 'Нью Йорк']}
+
+
+print("посещал города: student['cities']") # посещал города: ['Лондон', 'Париж', 'Нью Йорк']
---
+++ slovar.py
@@ -0,0 +1,7 @@
+account = ['login':'sveta', 'password':'12345']
+user = ['name':'piska','age':23,'account': account]
+
+account1 = ['login':'ivan', 'password':'q1']
+account2 = ['login':'petr', 'password':'q2']
+account3 = ['login':'olga', 'password':'qq5']
+account4 = ['login':'anna', 'password':'qqq4']
