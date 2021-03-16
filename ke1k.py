2bc13dd1f07ffd4d65f29c08df4210b04539ab92
RABBITPYL9 <shubins317@gmail.com>
2021-03-16 15:01:34 +0300

    THIS IS SPARTA

--- ke1k.py
+++ ke1k.py
@@ -1,9 +1,8 @@
 def get_absolute_url(url, *args, **kwargs):
     for i in args:
         url += '/' + i
-        url += '?'
     for k, v in kwargs.items():
-        url += k + '=' + str(v) + '&'
+        url += '&' + k + '=' + str(v)
     #return get_absolute_url(url, args, kwargs)
     return url
 
