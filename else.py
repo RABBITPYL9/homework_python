d1e41b9fe4a0341e36be64410a43a3aa4c60c341
RABBITPYL9 <shubins317@gmail.com>
2021-03-16 15:20:59 +0300

    THIS IS CTACOK!

---
+++ else.py
@@ -0,0 +1,45 @@
+def get_absolute_url (url, *args, **kwargs):
+
+
+    for i in args:
+
+       
+
+        url += '/' + i
+
+    
+
+    url += '?'
+
+    
+
+    for k, v in kwargs.items():
+
+        
+
+        url += k + '='+ v +'&'
+
+    
+
+    url = url[:-1] 
+
+        
+
+    
+
+    return url
+
+        
+
+        
+
+print(get_absolute_url('www.yandex.ru', 'posts', 'news', id='24', author='admin'))
+
+print(get_absolute_url('www.google.com', 'images', id='24', category='auto', color='red', size='small'))
+
+
+
+# www.yandex.ru/posts/news?id=24&author=admin
+
+# www.google.com/images?id=24&category=auto&color=red&size=small
+
--- get_url.py
+++ get_url.py
@@ -1,10 +1,11 @@
 def get_absolute_url(url, *args, **kwargs):
     for i in args:
         url += '/' + i
-        url += '?'
+    url += '?'
     for k, v in kwargs.items():
         url += k + '=' + str(v) + '&'
     #return get_absolute_url(url, args, kwargs)
+    url = url[:-1] 
     return url
 
 print(get_absolute_url('www.yandex.ru', 'posts', 'news', id=24, author='admin'))
