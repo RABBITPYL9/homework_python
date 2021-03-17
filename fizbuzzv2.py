e541fa31136b7367da051c6c4b50716a2fa6e0c8
RABBITPYL9 <shubins317@gmail.com>
2021-03-17 16:59:49 +0300

    kekski

---
+++ fizbuzzv2.py
@@ -0,0 +1,18 @@
+spisok = []
+#генерим числа от 1000 до 20000 включительно
+diapa = 0
+for i in range(1000, 20001):
+    diapa+=i
+    #print(i, end=',')
+    spisok.append(i) #добавляем в наш список
+
+
+#spisok.append(diapa)
+
+print(spisok)
+#вторая часть программы, которая проходит по всему списку spisok и по условию находит числа, которые делятся на 3 и 5
+#кладя их в переменную result
+for it in spisok:
+    if it%5 == 0 and it%3 ==0:
+        print(it)
+
